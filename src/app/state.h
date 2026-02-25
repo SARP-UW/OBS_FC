5e39f2bfe891f19948eef9e2db8e7dd4181ffd37
Charles Faisandier <faisandiercharles@outlook.com>
2026-02-25 13:04:26 -0800

    small reset

--- src/app/state.h
+++ src/app/state.h
@@ -5,24 +5,13 @@
 #pragma once
 #include <stdbool.h>
 
-#define NUM_STATES 8
-
 typedef struct {
-
-    bool (*build)(void); // setup info, if needed; setup info size
-    /**
-     * blocks until the current state ends
-     * The returned int is the index of the state in the state_machine.c file where
-     * the program should go to next.
-     *
-     * returning -1 makes the state machine exit after this state finishes
-     */
+    bool (*build)(void); 
     int (*run)(void);
-    bool (*destroy)(void); // called by state machine
+    bool (*destroy)(void); 
 } state;
 
 // State declarations / promises
-// Check corresponding .c files for details.
 extern const state init_state;
 extern const state standby_state;
 extern const state filling_state;
@@ -31,4 +20,42 @@ extern const state armed_state;
 extern const state firing_state;
 extern const state safe_state;
 extern const state recover_state;
+
+//     -- ( ) INITIALIZATION: First State, triggered upon power up
+//     ~~~ ( ) initializes everything
+//     ~~~ ( ) Establish radio and umbillical communication
+//     ~~~ ( ) Check battery health and power switching?
+//     ~~~ ( ) Verify all actuators and valves in safe position
+// 
+//     -- ( ) STANDBY: Rocket on pad awaiting instruction
+//     ~~~ ( ) Monitor ambient tank pressure and tempreature
+//     ~~~ ( ) Await the start fill command 
+//     ~~~ ( ) Remote venting capability is active to safeguard system
+// 
+//     --- ( ) FILLING: Remote delivery of fuel
+//     ~~~ ( ) Actuate GSE Valves to load propellant
+//     ~~~ ( ) Monitor mass flow and pressure 
+//     ~~~ ( ) If overpressurization is detected, auto abort to SAFE/ABORT
+// 
+//     - ( ) HOLD: Maintain launch ready state
+//     ~~~ ( ) Monitor leaks or pressure drops
+//     ~~~ ( ) Keep GSE Connected for power/data
+//     ~~~ ( ) Monitoro nitros phase changes
+// 
+//     -- ( ) ARMED: Final Countdown Sequence and umbilical disconnect
+//     ~~~ ( ) switch power from umbilical to battery 
+//     ~~~ ( ) disconnect umbillical
+// 
+//     -- ( ) FIRING: Deliver propelants to manifold
+//     ~~~ ( ) Open main propelant valves
+//     ~~~ ( ) Execute control loop for engine burn
+//     ~~~ ( ) REcord telemetry to Sd card
+// 
+//     -- ( ) SAFE/ABORT: Venting to abort mission
+//     ~~~ ( ) Vent everything 
+// 
+//     - ( ) AFTER/RECOVERY
+//     ~~~ ( ) close all valves to prevent leakages
+//     ~~~ ( ) stop sending data to ground, maybe location
+
 
