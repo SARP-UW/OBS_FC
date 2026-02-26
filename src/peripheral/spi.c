da07cfea813fdee2c2b35f84d4569f3e124caa43
Mahir Emran <mahiremran7@gmail.com>
2026-02-26 11:48:26 -0800

    Update spi.c

--- src/peripheral/spi.c
+++ src/peripheral/spi.c
@@ -1,4 +1,23 @@
-// Written by Jude Merritt
+/**
+ * This file is part of the Titan Flight Computer Project
+ * Copyright (c) 2026 UW SARP
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @file peripheral/spi.c
+ * @authors Jude Merritt
+ * @brief Implementation of SPI driver interface
+ */
 
 #include "peripheral/spi.h"
 #include "internal/mmio.h"
