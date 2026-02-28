4a103795edf880fcf89460cf1cbc33f200571d63
Jude Merritt <jm@Judes-MacBook-Air-559.local>
2026-02-28 03:28:50 -0800

    updated spi testing

--- test/test_spi.c
+++ test/test_spi.c
@@ -1,23 +1,19 @@
-#include "peripheral/gpio.h"
-#include "peripheral/watchdog.h"
-#include "internal/alloc.h"
-#include "peripheral/uart.h"
-#include "peripheral/pwm.h"
 #include "peripheral/spi.h"
-#include "peripheral/systick.h"
 #include <stdio.h>
 
 void test_spi() {
-	uint8_t instance = 3;
-	int result_init = spi_init(instance);
-	asm("BKPT #0");
+	int inst      = 1;
+	int ss_pins[1] = {43};
 
-	uint8_t source = 0b10101010;
-	uint8_t dest = 0;
+	spi_init(inst, ss_pins, 1);
+
+	uint8_t src[1] = {0xAA};
+	uint8_t dst[1] = {0x00};
 
 	while (1) {
-		int result_transfer = spi_transfer_sync(instance, &source, &dest, 1);
-		asm("BKPT #0");
+			asm("BKPT #0");
+			int result = spi_transfer_sync(inst, ss_pins[0], src, dst, 1);
+			asm("BKPT #0");
 	}
 }
 
